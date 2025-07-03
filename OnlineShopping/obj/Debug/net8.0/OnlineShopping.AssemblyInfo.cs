ee3e692b4c3d16d4d9512cfaf5aa69d44b9137b0
aysel982 <ayselim-bp110@code.edu.az>
2025-07-03 11:31:48 +0300

    Added new item

--- OnlineShopping/obj/Debug/net8.0/OnlineShopping.AssemblyInfo.cs
+++ OnlineShopping/obj/Debug/net8.0/OnlineShopping.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShopping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c442acf052989bcac57d17dc3879fe7e42fe14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6dcf1eca475645bef65d860ecc63c3fcbb9eed")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShopping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShopping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
